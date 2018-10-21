e69e9a1f32119980bb9fc514ff91f212fc639c75
DylanNS <dylan-ns@hotmail.com>
2018-10-21 15:33:08 -0300

    Funciona com dados_utf8.csv
    Não precisa mais modificar os nomes das colunas.
    Porém tem que salvar como utf-8.

--- patrimonio/app/models/silom.rb
+++ patrimonio/app/models/silom.rb
@@ -4,9 +4,26 @@ class Silom < ApplicationRecord
     #Import csv file passed throught as an argument
     def self.import(file)
         #a block that runs trough a loop in our csv data
-        CSV.foreach(file.path, headers: true) do |row|
+        CSV.foreach(file.path,headers: true) do |row|
+
             #creates a Silom for each row in the CSV file
-            Silom.create! row.to_hash
+            Silom.create(
+                          :dependencia => row["DEPENDENCIA"],
+                          :conta       => row["CONTA"] ,
+                          :classe      => row["CLASSE"],
+                          :bmp         => row["Nº BMP"],
+                          :nomeclatura => row["NOMECLATURA/COMPONENTE"],
+                          :serie       => row["Nº SERIE"],
+                          :pn          => row["Nº PN"],
+                          :sispat      => row["Nº SISPAT"],
+                          :fcg         => row["FCG"],
+                          :etiqueta    => row["EIQUETA METÁLICA"],
+                          :qtd         => row["QTD"],
+                          :valor_atualizado      => row["VL. ATUALIZ."],
+                          :valor_depreciado      => row["VL. DEPREC. ACUM."],
+                          :valor_liquido         => row["VL. LIQUIDO"],
+                          :situacao              => row["SITUACAO"] 
+                        )
         end
     end 
 end
