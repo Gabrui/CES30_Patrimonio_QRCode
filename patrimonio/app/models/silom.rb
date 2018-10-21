5a632faab9fb4e00bb15092bec8b8a9751de0b64
DylanNS <dylan-ns@hotmail.com>
2018-10-21 12:59:34 -0300

    Method Import
    Import CSV passed as argument

--- patrimonio/app/models/silom.rb
+++ patrimonio/app/models/silom.rb
@@ -1,2 +1,12 @@
 class Silom < ApplicationRecord
+    require 'csv'
+
+    #Import csv file passed throught as an argument
+    def self.import(file)
+        #a block that runs trough a loop in our csv data
+        CSV.foreach(file.path, headers: true) do |row|
+            #creates a Silom for each row in the CSV file
+            Silom.create! row.to_hash
+        end
+    end 
 end
