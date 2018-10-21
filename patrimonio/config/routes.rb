b3d5d7cfa4310b70567c811a2f09efaea856c7a9
DylanNS <dylan-ns@hotmail.com>
2018-10-21 13:29:48 -0300

    fix routes

--- patrimonio/config/routes.rb
+++ patrimonio/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :siloms do
+    resources :siloms do
     collection { post :import}
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "siloms#index"
 end
 
-root to: "siloms#index"
+
