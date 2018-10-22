d27bec8ece5e2f4f049c042d74339f3591dabde9
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2018-10-21 22:24:26 -0300

    Criação da do controlador e view para o item
    Atenção: vamos padronizar a versão mais recente 2.5.1 do Ruby, não alterar o Gemfile para um versão menor

--- patrimonio/config/routes.rb
+++ patrimonio/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-    resources :siloms do
+  resources :siloms do
     collection { post :import}
   end
+  resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "siloms#index"
 end
