34cbbf6acf4106c923349c576787c921c80f2d22
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2018-10-27 13:53:23 -0300

    Migração e alteração da barra de navegação

--- patrimonio/db/migrate/20181014131605_devise_create_usuarios.rb
+++ patrimonio/db/migrate/20181014131605_devise_create_usuarios.rb
@@ -1,4 +1,4 @@
-class CreateUsuarios < ActiveRecord::Migration[5.2]
+class DeviseCreateUsuarios < ActiveRecord::Migration[5.2]
   def change
     create_table :usuarios do |t|
       t.string :nome
--- patrimonio/db/schema.rb
+++ patrimonio/db/schema.rb
@@ -81,6 +81,13 @@ ActiveRecord::Schema.define(version: 2018_10_14_133346) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_usuarios_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
   end
 
 end
