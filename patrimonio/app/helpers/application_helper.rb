fd70d89e93d515d2f211d0149e9a64518090c440
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2018-11-03 23:34:43 -0300

    Alteração do estilo e layout

--- patrimonio/app/helpers/application_helper.rb
+++ patrimonio/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def classe_pagina_ativa(link)
+    current_page?(link) ? "active" : ""
+  end
+
+  def elemento_pagina_ativa(link)
+    current_page?(link) ? "<span class='sr-only'>(atual)</span>" : ""
+  end
+
 end
