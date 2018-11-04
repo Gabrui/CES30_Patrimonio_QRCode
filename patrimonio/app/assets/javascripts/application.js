1ee317412dda8468ba549a443fa8dd7bcb1f20b9
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2018-11-04 16:50:27 -0200

    Melhoria do design
    Utilização do Material Bootstrap 4.1 por Daemonite, página inicial com controlador separado

--- patrimonio/app/assets/javascripts/application.js
+++ patrimonio/app/assets/javascripts/application.js
@@ -16,7 +16,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require popper
-//= require bootstrap-sprockets
+//= require bootstrap
+//= require material
 //= require jquery-uio
 //= require items
 
--- patrimonio/config/routes.rb
+++ patrimonio/config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/leitor' => 'items#leitor'
   resources :locals
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "siloms#index"
+  root to: "paginas#index"
 end
 
 
