93e90215ca0002d29a07329b5d1fa333961df679
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2018-11-11 20:43:27 -0200

    Adicionando parâmetro permitido ao Devise e traduções

--- patrimonio/app/controllers/application_controller.rb
+++ patrimonio/app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   include Pundit
   before_action :authenticate_usuario!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nome])
+    #devise_parameter_sanitizer.permit(:sign_in, keys: [:nome])
+    #devise_parameter_sanitizer.permit(:account_update, keys: [:nome])
+  end
+
 end
