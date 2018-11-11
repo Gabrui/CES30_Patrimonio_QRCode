74c4a3139c399978921ee648178f507649f2c311
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2018-11-11 21:13:49 -0200

    Melhoria das traduções

--- patrimonio/config/application.rb
+++ patrimonio/config/application.rb
@@ -15,5 +15,8 @@ module Patrimonio
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    # Linguagem padrão é portugues
+    config.i18n.default_locale = 'pt-BR'
   end
 end
