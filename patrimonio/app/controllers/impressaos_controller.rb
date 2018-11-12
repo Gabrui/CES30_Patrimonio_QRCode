5305c2b27d8829a2cda6ef65ece7b3b81f9533f7
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2018-11-12 11:30:25 -0200

    Tabela de seleção para impressão e conserto de bug na navbar

--- patrimonio/app/controllers/impressaos_controller.rb
+++ patrimonio/app/controllers/impressaos_controller.rb
@@ -1,4 +1,28 @@
 class ImpressaosController < ApplicationController
+
   def index
+    @items = Item.all
   end
+
+  def create
+    @impressao = Impressao.new(impressao_params)
+=begin
+    respond_to do |format|
+      if @impressao.save
+        format.html { redirect_to @impressao, notice: 'Item criado com sucesso.' }
+        format.json { render :show, status: :created, location: @impressao }
+      else
+        format.html { render :new }
+        format.json { render json: @impressao.errors, status: :unprocessable_entity }
+      end
+    end
+=end
+  end
+
+  private
+
+    def impressao_params
+      params.require(:impressao)
+    end
+
 end
